ed73f5dece01e0e23d30c3de13ed3936e6ba90a8
Anca Zamfir <zamfiranca@gmail.com>
2025-03-26 12:53:07 +0100

    Generate proposal for PartsOnly in the engine

--- code/crates/core-consensus/src/handle/proposed_value.rs
+++ code/crates/core-consensus/src/handle/proposed_value.rs
@@ -4,7 +4,6 @@ use crate::handle::driver::apply_driver_input;
 use crate::types::ProposedValue;
 
 use super::decide::decide_current_no_timeout;
-use super::signature::sign_proposal;
 
 pub async fn on_proposed_value<Ctx>(
     co: &Co<Ctx>,
@@ -33,25 +32,6 @@ where
     }
 
     state.store_value(&proposed_value);
-
-    // There are two cases where we need to generate an internal Proposal message for consensus to process the full proposal:
-    // a) In parts-only mode, where we do not get a Proposal message but only the proposal parts
-    // b) In any mode if the proposed value was provided by Sync, where we do net get a Proposal message but only the full value and the certificate
-    if state.params.value_payload.parts_only() || origin == ValueOrigin::Sync {
-        let proposal = Ctx::new_proposal(
-            proposed_value.height,
-            proposed_value.round,
-            proposed_value.value.clone(),
-            proposed_value.valid_round,
-            proposed_value.proposer.clone(),
-        );
-
-        // TODO: Keep unsigned proposals in keeper.
-        // For now we keep all happy by signing all "implicit" proposals with this node's key
-        let signed_proposal = sign_proposal(co, proposal).await?;
-
-        state.store_proposal(signed_proposal);
-    }
 
     let proposals = state.full_proposals_for_value(&proposed_value);
     for signed_proposal in proposals {
