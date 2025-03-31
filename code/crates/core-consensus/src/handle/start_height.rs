434cc37321213cf1a8c47deee82e42aa079e4f54
Romain Ruetschi <romain@informal.systems>
2025-03-31 14:36:40 +0200

    Fix unused variable warning

--- code/crates/core-consensus/src/handle/start_height.rs
+++ code/crates/core-consensus/src/handle/start_height.rs
@@ -19,6 +19,9 @@ where
     #[cfg(feature = "metrics")]
     metrics.step_end(state.driver.step());
 
+    #[cfg(not(feature = "metrics"))]
+    let _ = metrics;
+
     state.driver.move_to_height(height, validator_set);
 
     debug_assert_eq!(state.driver.height(), height);
