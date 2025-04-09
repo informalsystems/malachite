0662a6abeb8979f0c97550d56de0aad4a3390bb8
Anca Zamfir <zamfiranca@gmail.com>
2025-04-09 19:58:14 +0200

    Generate proposal for PartsOnly in the engine

--- code/crates/core-consensus/src/handle/proposed_value.rs
+++ code/crates/core-consensus/src/handle/proposed_value.rs
@@ -4,7 +4,6 @@ use crate::handle::driver::apply_driver_input;
 use crate::types::{ProposedValue, WalEntry};
 
 use super::decide::try_decide;
-use super::signature::sign_proposal;
 
 /// Handles a proposed value that can originate from multiple sources:
 /// 1. Application layer:
@@ -54,26 +53,6 @@ where
         );
 
         return Ok(());
-    }
-
-    // There are two cases where we need to generate an internal Proposal message for consensus to process the full proposal:
-    // a) In parts-only mode, where we do not get a Proposal message but only the proposal parts
-    // b) In any mode if the proposed value was provided by Sync, where we do net get a Proposal message but only the full value and the certificate
-    if state.params.value_payload.parts_only() || origin == ValueOrigin::Sync {
-        let proposal = Ctx::new_proposal(
-            &state.ctx,
-            proposed_value.height,
-            proposed_value.round,
-            proposed_value.value.clone(),
-            proposed_value.valid_round,
-            proposed_value.proposer.clone(),
-        );
-
-        // TODO: Keep unsigned proposals in keeper.
-        // For now we keep all happy by signing all "implicit" proposals with this node's key
-        let signed_proposal = sign_proposal(co, proposal).await?;
-
-        state.store_proposal(signed_proposal);
     }
 
     // If this is the first time we see this value, append it to the WAL, so it can be used for recovery.
