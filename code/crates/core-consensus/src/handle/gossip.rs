5a2068beb1d1b6c71ce86fdcf1a3065579949120
Anca Zamfir <zamfiranca@gmail.com>
2025-05-02 13:15:38 -0400

    Add the new gossip and msg handling files

---
+++ code/crates/core-consensus/src/handle/gossip.rs
@@ -0,0 +1,118 @@
+use crate::handle::driver::apply_driver_input;
+use crate::handle::validator_set::get_validator_set;
+use crate::prelude::*;
+
+use super::signature::{verify_polka_certificate, verify_round_certificate};
+
+pub async fn on_polka_certificate<Ctx>(
+    co: &Co<Ctx>,
+    state: &mut State<Ctx>,
+    metrics: &Metrics,
+    certificate: PolkaCertificate<Ctx>,
+) -> Result<(), Error<Ctx>>
+where
+    Ctx: Context,
+{
+    info!(%certificate.height, %certificate.round, "Received polka certificate");
+
+    if certificate.height != state.height() {
+        warn!(
+            %certificate.height,
+            consensus.height = %state.height(),
+            "Polka certificate height mismatch"
+        );
+
+        return Ok(());
+    }
+
+    let validator_set = get_validator_set(co, state, certificate.height)
+        .await?
+        .ok_or_else(|| Error::ValidatorSetNotFound(certificate.height))?;
+
+    let validity = verify_polka_certificate(
+        co,
+        certificate.clone(),
+        validator_set.into_owned(),
+        state.params.threshold_params,
+    )
+    .await?;
+
+    if let Err(e) = validity {
+        warn!(?certificate, "Invalid polka certificate: {e}");
+        return Ok(());
+    }
+
+    apply_driver_input(
+        co,
+        state,
+        metrics,
+        DriverInput::PolkaCertificate(certificate),
+    )
+    .await
+}
+
+pub async fn on_round_certificate<Ctx>(
+    co: &Co<Ctx>,
+    state: &mut State<Ctx>,
+    metrics: &Metrics,
+    certificate: RoundCertificate<Ctx>,
+) -> Result<(), Error<Ctx>>
+where
+    Ctx: Context,
+{
+    info!(%certificate.height, %certificate.round, "Received round certificate");
+
+    if certificate.height != state.height() {
+        warn!(
+            %certificate.height,
+            consensus.height = %state.height(),
+            "Polka certificate height mismatch"
+        );
+
+        return Ok(());
+    }
+
+    let validator_set = get_validator_set(co, state, certificate.height)
+        .await?
+        .ok_or_else(|| Error::ValidatorSetNotFound(certificate.height))?;
+
+    let validity = verify_round_certificate(
+        co,
+        certificate.clone(),
+        validator_set.into_owned(),
+        state.params.threshold_params,
+    )
+    .await?;
+
+    if let Err(e) = validity {
+        warn!(?certificate, "Invalid round certificate: {e}");
+        return Ok(());
+    }
+
+    for signature in certificate.round_signatures {
+        let vote_type = signature.vote_type;
+        let vote: SignedVote<Ctx> = match vote_type {
+            VoteType::Prevote => SignedVote::new(
+                state.ctx.new_prevote(
+                    certificate.height,
+                    certificate.round,
+                    signature.value_id,
+                    signature.address,
+                ),
+                signature.signature,
+            ),
+            VoteType::Precommit => SignedVote::new(
+                state.ctx.new_precommit(
+                    certificate.height,
+                    certificate.round,
+                    signature.value_id,
+                    signature.address,
+                ),
+                signature.signature,
+            ),
+        };
+
+        apply_driver_input(co, state, metrics, DriverInput::Vote(vote)).await?;
+    }
+    Ok(())
+}
