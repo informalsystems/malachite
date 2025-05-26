e5353ba3b20f99c08fd48e76864c26d467ff4465
Romain Ruetschi <romain@informal.systems>
2025-05-26 16:00:41 +0200

    Cleanup

--- code/crates/starknet/host/src/mempool.rs
+++ code/crates/starknet/host/src/mempool.rs
@@ -7,7 +7,6 @@ use ractor::{Actor, ActorProcessingErr, ActorRef, RpcReplyPort};
 use rand::RngCore;
 use tracing::{debug, info, trace};
 
-use malachitebft_starknet_p2p_proto as p2p_proto;
 use malachitebft_test_mempool::types::MempoolTransactionBatch;
 use malachitebft_test_mempool::{Event as NetworkEvent, NetworkMsg, PeerId};
 
