informalsystems/malachite

commit b22f678f05b7414c77d7d2cf7b5d2231e67038d8

Small refactor

code/crates/sync/src/handle.rs
@@ -407,25 +407,28 @@ where
     Ctx: Context,
 {
     let mut height = state.sync_height;
+
     let limit = state
         .sync_height
         .increment_by(state.config.parallel_requests);
+
     loop {
-        // Request sync from a peer if we do not have any pending request or validation for this height.
-        if !state.has_pending_value_request(&height) && !state.has_pending_value_validation(&height)
-        {
-            if let Some(peer) = state.random_peer_with_tip_at_or_above(height) {
-                request_value_from_peer(&co, state, metrics, height, peer).await?;
-            } else {
-                debug!(height.sync = %height, "No peer to request sync from");
-                // No peer reached this height yet, we can stop here.
-                break;
-            }
-        } else {
+        // Check if already have a pending request or validation for this height.
+        if state.has_pending_value_request(&height) || state.has_pending_value_validation(&height) {
             debug!(height.sync = %height, "Already have a pending request or validation for this height");
+            continue;
         }
 
+        let Some(peer) = state.random_peer_with_tip_at_or_above(height) else {
+            debug!(height.sync = %height, "No peer to request sync from");
+            // No peer reached this height yet, we can stop here.
+            break;
+        };
+
+        request_value_from_peer(&co, state, metrics, height, peer).await?;
+
         height = height.increment();
+
         if height >= limit {
             break;
         }

commit d547423982fc30099e88490cf1db4b60674467ac

Fix bug introduced in b22f678f

code/crates/sync/src/handle.rs
@@ -414,18 +414,18 @@ where
 
     loop {
         // Check if already have a pending request or validation for this height.
-        if state.has_pending_value_request(&height) || state.has_pending_value_validation(&height) {
+        if !state.has_pending_value_request(&height) && !state.has_pending_value_validation(&height)
+        {
+            let Some(peer) = state.random_peer_with_tip_at_or_above(height) else {
+                debug!(height.sync = %height, "No peer to request sync from");
+                // No peer reached this height yet, we can stop here.
+                break;
+            };
+
+            request_value_from_peer(&co, state, metrics, height, peer).await?;
+        } else {
             debug!(height.sync = %height, "Already have a pending request or validation for this height");
-            continue;
         }
-
-        let Some(peer) = state.random_peer_with_tip_at_or_above(height) else {
-            debug!(height.sync = %height, "No peer to request sync from");
-            // No peer reached this height yet, we can stop here.
-            break;
-        };
-
-        request_value_from_peer(&co, state, metrics, height, peer).await?;
 
         height = height.increment();
 